b232f970a4342ec5e92bdab37bf0de62717de699
Amanibus <paulschwerdtner@web.de>
2021-11-10 20:20:22 +0100

    add create gugercin models pipeline

--- system_pipelines/create_gugercin_models.jl
+++ system_pipelines/create_gugercin_models.jl
@@ -1,8 +1,11 @@
-using PortHamiltonianBenchmarkSystems, HDF5
+using PortHamiltonianBenchmarkSystems, MAT
 
 for n_cells in [50, 500, 10_000]
-  J, R, Q, B = gugercin_pH_msd_chain(n_cells = n_cells)
-  h5open("GugercinN$(2*n_cells).h5", "w") do file
-    write(file, "J", J, "R", Q, "Q", B, "B")
-  end
+  J, R, Q, B = PortHamiltonianBenchmarkSystems.gugercin_pH_msd_chain(n_cells = n_cells)
+  matwrite("GugercinN$(2*n_cells).mat", Dict(
+       "J" => J,
+       "R" => R,
+       "Q" => Q,
+       "B" => B,
+      ))
 end
