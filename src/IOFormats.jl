042c05e12aa73483004c7074fefee553987b8e34
Amanibus <paulschwerdtner@web.de>
2021-11-11 10:27:57 +0100

    add reader functions for hdf5 and mat

--- src/IOFormats.jl
+++ src/IOFormats.jl
@@ -11,12 +11,33 @@ end
 m_write_hdf5(f::String, varname::String, var::Matrix) = h5write(f, varname, var)
 m_write_hdf5(f::String, varname::String, var::SparseMatrixCSC) = H5SparseMatrixCSC(f, varname, var)
 
+function loadHDF5(filename::String)
+  fid = h5open(filename)
+  d = Dict()
+  for key in keys(fid)
+    d[key] =  m_read(fid, key, fid[key])
+  end
+  return d
+end
+
+function m_read(::Any, ::Any, dset::HDF5.Dataset)
+  return read(dset)
+end
+function m_read(fid, key, dset::HDF5.Group)
+  return H5SparseMatrixCSC(fid, key) |> SparseMatrixCSC
+end
+
+
 function writeMAT(filename::String, data::Tuple{String, AbstractMatrix}...)
   d = Dict()
   for mat in data
     d[mat[1]] = mat[2]
   end
   matwrite(filename, d)
+end
+
+function loadMAT(filename::String)
+  return matread(filename)
 end
 
 export writeHDF5, writeMAT
