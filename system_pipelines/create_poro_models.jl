f9612e2c90ddc40caff794141aa422a966c06ec1
Amanibus <paulschwerdtner@web.de>
2021-11-15 16:18:02 +0100

    fix n_cells -> n, in create_poro_models pipeline

--- system_pipelines/create_poro_models.jl
+++ system_pipelines/create_poro_models.jl
@@ -1,6 +1,6 @@
 using PortHamiltonianBenchmarkSystems, MAT
 
-for n_cells in [320, 980, 1805]
+for n in [320, 980, 1805]
   E, J, R, B = poro_elasticity_model(n=n)
   filename = "PoroModelN$(2n_cells)"
   writeHDF5(filename*".h5"; E, J, R, B)
