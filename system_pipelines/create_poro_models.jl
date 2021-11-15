0f2c8db9ad52c526a8308c11efe87362155496b7
Amanibus <paulschwerdtner@web.de>
2021-11-15 13:04:50 +0100

    add model creation pipeline for poro models

---
+++ system_pipelines/create_poro_models.jl
@@ -0,0 +1,8 @@
+using PortHamiltonianBenchmarkSystems, MAT
+
+for n_cells in [320, 980, 1805]
+  E, J, R, B = poro_elasticity_model(n=n)
+  filename = "PoroModelN$(2n_cells)"
+  writeHDF5(filename*".h5"; E, J, R, B)
+  writeMAT(filename*".mat"; E, J, R, B)
+end
