861cdd28a9466cb30ebe511179c9c72f34fe352d
Algopaul <paulschwerdtner@web.de>
2022-04-13 13:09:01 +0200

    add deployment to makefile

--- docs/make.jl
+++ docs/make.jl
@@ -2,3 +2,6 @@ using Documenter, PortHamiltonianBenchmarkSystems
 
 push!(LOAD_PATH, "../src/")
 makedocs(sitename="PortHamiltonianBenchmarkSystems Documentation")
+deploydocs(
+  repo = "github.com/Algopaul/PortHamiltonianBenchmarkSystems.git"
+)
