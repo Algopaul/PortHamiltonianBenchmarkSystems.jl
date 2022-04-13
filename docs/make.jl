f047bccc0621fe0846995011c4fef01b641334ab
Algopaul <paulschwerdtner@web.de>
2022-04-13 13:19:17 +0200

    remove versioning

--- docs/make.jl
+++ docs/make.jl
@@ -3,5 +3,6 @@ using Documenter, PortHamiltonianBenchmarkSystems
 push!(LOAD_PATH, "../src/")
 makedocs(sitename="PortHamiltonianBenchmarkSystems Documentation")
 deploydocs(
-  repo = "github.com/Algopaul/PortHamiltonianBenchmarkSystems.git"
+  repo = "github.com/Algopaul/PortHamiltonianBenchmarkSystems.git",
+  versions = nothing
 )
