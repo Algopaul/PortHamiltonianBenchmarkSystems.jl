b506423f97df8cc406f1ae850801c7513f42e1f8
Algopaul <paulschwerdtner@web.de>
2022-07-21 13:32:43 +0200

    remove contribution info

--- docs/make.jl
+++ docs/make.jl
@@ -6,7 +6,7 @@ makedocs(
     sitename = "PortHamiltonianBenchmarkSystems",
     pages = [
         "Home" => "index.md",
-        "Contribution" => "contribution.md",
+        # "Contribution" => "contribution.md",
         "Benchmark Systems" =>
             ["GugercinMSDChain.md", "PoroModel.md", "RclCircuits.md", "DampedWaveNet.md"],
     ],
